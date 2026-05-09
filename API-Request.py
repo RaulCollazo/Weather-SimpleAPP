afc66bd2d454c4f2fc27a17af4dea815a69a7686
RaulCollazo <195011609+RaulCollazo@users.noreply.github.com>
2026-05-09 03:03:28 -0300

    Mejorada lectura de datos, cambiada unidad a celsius

--- API-Request.py
+++ API-Request.py
@@ -1,8 +1,18 @@
 import requests
 import json
 
-request = requests.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Argentina?unitGroup=us&include=current&key=U9MCVCSAG2JTLJBQH9FH8F27Q&contentType=json")
 
+def requestCiudad ():
+    ciudad = str(input("Por favor ingrese su ciudad: "))
+    params = {"unitGroup" : "metric" , "key" : "U9MCVCSAG2JTLJBQH9FH8F27Q" , "contentType" : "json" , "include" : "current"}
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ ciudad +"?unitGroup=metric&include=current&key=U9MCVCSAG2JTLJBQH9FH8F27Q&contentType=json"
+    request = requests.get(url , params = params)
+    return request
 
+estado = requestCiudad().json()
+estadoActual = estado["currentConditions"]
 
-print(json.dumps(request.json(), indent=4, ensure_ascii=False))
+print("Temperatura:", estadoActual["temp"])
+print("Condición:", estadoActual["conditions"])
+
+#print(json.dumps(requestCiudad().json(), indent=4, ensure_ascii=False))
